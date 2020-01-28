c83ff629aeb9fc198a54686cc1ba2e57cb482821
remi-gai <56744348+remi-gai@users.noreply.github.com>
2020-01-27 16:27:51 -0800

    completed the API for DB query and refactored functions in db.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.queryData();
+  }
+
+  queryData () {
+    axios.get('/restaurants')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
       <div>
