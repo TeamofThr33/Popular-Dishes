0630183c157cb6b713706a30dea091a4e0701f62
remi-gai <56744348+remi-gai@users.noreply.github.com>
2020-01-28 18:05:52 -0800

    resubmitted with a new branch

--- test/PopularDishes.test.js
+++ test/PopularDishes.test.js
@@ -3,9 +3,11 @@ import { shallow } from 'enzyme';
 import PopularDishes from '../client/src/components/PopularDishes';
 
 describe('checks for the rendering of Popular Dishes', () => {
+
     it('should contain "Hello Dish"', () => {
         const wrapper = shallow(<PopularDishes />);
         const divTitle = wrapper.find("div.title");
         expect(divTitle.text()).toEqual("Hello Dish");
     })
+    
 });
